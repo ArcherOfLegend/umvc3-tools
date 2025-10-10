676e99c9c6bef0959220dc2a1f0949bfe197f7c1
Archer <92534692+ArcherOfLegend@users.noreply.github.com>
2025-10-10 19:22:27 +0300

    Fix skeleton export bone parent resolution

--- src/python/mtio/modules/mtblender/umvc3_model_importer/modules/blender_exporter.py
+++ src/python/mtio/modules/mtblender/umvc3_model_importer/modules/blender_exporter.py
@@ -1,10 +1,11 @@
-from typing import Dict
+from typing import Dict, List
 from ..mtlib import *
 from ..mtlib.ncl import *
 from ..mtlib.base_editor import *
 from ..mtlib.base_exporter import *
 from .blender_plugin import *
 import bpy
+import mathutils
 
 def progressCallback( what, i, count ):
     pass
@@ -14,6 +15,61 @@ def assertBlenderMode(expectedMode:str):
         bpy.context.object.mode == expectedMode
     except AttributeError:
         return expectedMode == 'OBJECT'
+
+class BlenderExportBoneProxy(EditorNodeProxy):
+    def __init__(self, bone, editBone, tfm, lookup):
+        super().__init__(plugin)
+        self.node = bone
+        self._editBone = editBone
+        self._tfm = tfm
+        self._parent = None
+        self._lookup = lookup
+
+    def unwrap(self):
+        return self.node
+
+    def setParent(self, parent):
+        self._parent = parent
+
+    def getTransform(self):
+        return self._tfm
+
+    def getParent(self):
+        if self._parent is not None:
+            return self._parent
+
+        if self.node.parent is not None:
+            return self._lookup(self.node.parent.name)
+
+        return None
+
+    def getName(self):
+        return self.node.name
+
+    def isHidden(self):
+        if self._editBone is not None:
+            if hasattr(self._editBone, 'hide'):
+                return self._editBone.hide
+
+            return getattr(self._editBone, 'hide_select', False)
+
+        return getattr(self.node, 'hide', False)
+
+    def isBoneHidden(self):
+        return self.isHidden()
+
+    def isMeshNode(self):
+        return False
+
+    def isGroupNode(self):
+        return False
+
+    def isBoneNode(self):
+        return True
+
+    def isSplineNode(self):
+        return False
+
 
 class BlenderModelExporter(ModelExporterBase):
     def __init__(self) -> None:
@@ -30,19 +86,73 @@ class BlenderModelExporter(ModelExporterBase):
 
     #Based on the above method to ensure that we get joints as they are not included in bpy.data.objects.
     def getObjectBones( self ):
-        temp = list(bpy.data.objects)
-        objects = []
-        for o in temp:
-            if not o in self.processedNodes:
-                #Ensures we only get the bones from the Armature selected and adds all of them.
-                if o.type == 'ARMATURE' and o.name == bpy.context.selected_objects[0].name:
-                    # for ChildNode in enumerate(bpy.data.armatures[o.name].bones):
-                    for ChildNode in bpy.data.armatures[o.name].bones:
-                        #o.node = ChildNode
-                        objects.append( BlenderNodeProxy( ChildNode ) )
+        armatureObj = None
+        activeObj = bpy.context.view_layer.objects.active
 
+        if activeObj is not None and activeObj.type == 'ARMATURE':
+            armatureObj = activeObj
+        else:
+            for candidate in bpy.context.selected_objects:
+                if candidate.type == 'ARMATURE':
+                    armatureObj = candidate
+                    break
+
+        if armatureObj is None:
+            return []
+
+        previousActive = bpy.context.view_layer.objects.active
+        previousMode = armatureObj.mode
+        bpy.context.view_layer.objects.active = armatureObj
+
+        if armatureObj.mode != 'EDIT':
+            bpy.ops.object.mode_set(mode='EDIT', toggle=False)
+
+        tailRotationMatrix = mathutils.Matrix(((0.0, 1.0, 0.0, 0.0),
+                                               (-1.0, 0.0, 0.0, 0.0),
+                                               (0.0, 0.0, 1.0, 0.0),
+                                               (0.0, 0.0, 0.0, 1.0)))
+        tailRotationMatrixInv = tailRotationMatrix.inverted()
+
+        editBones = {editBone.name: editBone for editBone in armatureObj.data.edit_bones}
+        boneProxyByName: Dict[str, BlenderExportBoneProxy] = dict()
+
+        def lookup(name: str):
+            return boneProxyByName.get(name)
+
+        objects: List[BlenderExportBoneProxy] = []
+
+        for bone in armatureObj.data.bones:
+            if bone.name not in editBones:
+                continue
+
+            editBone = editBones[bone.name]
+            matrix = editBone.matrix.copy()
+            matrix = matrix @ tailRotationMatrixInv
+            tfm = self.convertMatrix3ToNclMat44(matrix)
+
+            proxy = BlenderExportBoneProxy(bone, editBone, tfm, lookup)
+            boneProxyByName[bone.name] = proxy
+            objects.append(proxy)
+
+        for proxy in objects:
+            parent = proxy.node.parent
+            if parent is not None:
+                proxy.setParent(boneProxyByName.get(parent.name))
+
+        if previousMode != 'EDIT':
+            bpy.ops.object.mode_set(mode=previousMode, toggle=False)
+
+        if previousActive is not None:
+            bpy.context.view_layer.objects.active = previousActive
 
         return objects
+
+    def iterBoneNodes(self):
+        for editorNode in self.getObjectBones():
+            if not self.shouldExportNode(editorNode):
+                continue
+
+            yield editorNode
 
     # def getObjectBones( self ):
     #     temp = list(bpy.data.objects)
